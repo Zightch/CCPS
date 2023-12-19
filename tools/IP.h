ef391a007117c72f123246e1f5a32f636236e8ac
Zightch <Zightch@163.com>
2023-12-19 20:47:11 +0800

    优化代码2

--- tools/IP.h
+++ tools/IP.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#ifdef _WIN32
+#define CCPS_DLL __declspec(dllexport)
+#elif __linux__
+#define CCPS_DLL
+#endif
+
 class QByteArray;
 class QHostAddress;
 
-QByteArray IPPort(const QHostAddress &, unsigned short);
+QByteArray CCPS_DLL IPPort(const QHostAddress &, unsigned short);
