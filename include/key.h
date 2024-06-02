ad8bada3871fd7ede4f81b28e3f719b2fc4e605b
Zightch <Zightch@163.com>
2024-06-02 22:39:51 +0800

    .

--- include/key.h
+++ include/key.h
@@ -17,12 +17,13 @@
 #define KEY_DLL C_IDENTIFY
 #endif
 
-#define X25519_LEN 32
+#define LEN_25519 32
 #define IV_LEN 16
 
 KEY_DLL int genKeyPair(unsigned char *priKey, unsigned char *pubKey);
 KEY_DLL int genSharedKey(unsigned char *priKey, unsigned char *pubKey, unsigned char *sharedKey);
 KEY_DLL int encryptData(unsigned char *msg, int msgSize, unsigned char *key, unsigned char *IV, unsigned char *cipher);
 KEY_DLL int decryptData(unsigned char *cipher, int cipherSize, unsigned char *key, unsigned char *IV, unsigned char *msg);
+KEY_DLL int verify(unsigned char *pubKey, unsigned char *message, long long msgSize, unsigned char *sign, long long sigSize);
 
 #endif
